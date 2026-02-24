15c7b66de979855f83dcd435ffd3430764f19693
LEVI JOSE DE MELO AGUIAR <levi.jmaguiar@senacsp.edu.br>
2026-02-23 22:15:11 -0300

    Atualização 23.02.2026

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,36 @@
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 
-import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
+import { Image, StyleSheet, Text, View, ScrollView, KeyboardAvoidingView, Platform, Alert} from "react-native";
 import { Link } from "expo-router";
+import { useState } from "react";
 
 export default function Index(){
-    return(
+    const [email, setEmail] = useState("");
+    function handleSignIn(){
+        console.log(email)
+        Alert.alert("Entrar", "Preencha e-mail e senha para entrar!")
+    }    return(
+        <KeyboardAvoidingView
+            style={{flex:1}}
+            behavior={Platform.select({ios:"padding", android:"height"})}
+        >
         <ScrollView contentContainerStyle={{ flexGrow:1 }}>
             <View style={styles.container}>
                 <Image 
-                    source={require('@/assets/image1.png')}
+                    source={require('@/assets/image1.gif')}
                     style={styles.ilustration} 
                 />
+                <Text style={styles.title}>Entrar {email}</Text>
+                <Text style={styles.subtitle}>Faça seu login</Text>
                 <View style={styles.form}>
-                    <Input placeholder="E-mail" keyboardType="email-address" />
+                    <Input placeholder="E-mail" 
+                        keyboardType="email-address"
+                        // onChangeText={(text) => console.log(text)}
+                        onChangeText={setEmail}
+                        />
                     <Input placeholder="Senha" secureTextEntry/>
-                    <Button label="Entrar" />
+                    <Button label="Entrar" onPress={handleSignIn} />
                     {/* <Button label="Entrar" style={{ backgroundColor: "green"}}/> */}
                 </View>
                 <Text style={styles.footerText}>Não tem uma conta? 
@@ -25,6 +40,7 @@ export default function Index(){
                 </Text>
             </View>
         </ScrollView>
+        </KeyboardAvoidingView>
     )
 } 
 
@@ -52,5 +68,13 @@ const styles = StyleSheet.create({
     form: {
         marginTop:24,
         gap:12
+    },
+    title:{
+        fontSize:32,
+        fontWeight: 900,
+        color:"#505050ff"
+    },
+    subtitle:{
+        fontSize:16,
     }
 })
--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,13 +1,73 @@
-import { Text, View } from "react-native";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+
+import { Image, StyleSheet, Text, View, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import { Link } from "expo-router";
 
 export default function Signup(){
     return(
-        <View style={{
-            flex:1, 
-            justifyContent: "center", 
-            alignItems: "center" }}
+        <KeyboardAvoidingView
+            style={{flex:1}}
+            behavior={Platform.select({ios:"padding", android:"height"})}
         >
-        <Text >oi</Text>
-        </View>
+        <ScrollView contentContainerStyle={{ flexGrow:1 }}>
+            <View style={styles.container}>
+                <Image 
+                    source={require('@/assets/Cad.gif')}
+                    style={styles.ilustration}
+                />
+                <Text style={styles.title}>Cadastrar</Text>
+                <Text style={styles.subtitle}>Crie sua conta aqui</Text>
+                <View style={styles.form}>
+                    <Input placeholder="Nome" />
+                    <Input placeholder="E-mail" keyboardType="email-address" />
+                    <Input placeholder="Senha" secureTextEntry/>
+                    <Input placeholder="Confirmar Senha" secureTextEntry/>
+                    <Button label="Cadastrar" />
+                    {/* <Button label="Entrar" style={{ backgroundColor: "green"}}/> */}
+                </View>
+                <Text style={styles.footerText}>Já possui cadastro?
+                    <Link href="/" style={styles.footerLink}>
+                        {" "}Voltar ao menu
+                    </Link>
+                </Text>
+            </View>
+        </ScrollView>
+        </KeyboardAvoidingView>
     )
-}
+} 
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#FDFDFD",
+        padding:32
+    },
+    ilustration:{
+        width: "100%",
+        height: 330,
+        resizeMode:"contain",
+        marginTop:62
+    }, 
+    footerText:{
+        textAlign:"center",
+        marginTop:24,
+        color:"#585860",
+    },
+    footerLink:{
+        color:"#0929b8",
+        fontWeight:700
+    },
+    form: {
+        marginTop:24,
+        gap:12
+    },
+    title:{
+        fontSize:32,
+        fontWeight: 900,
+        color:"#505050ff"
+    },
+    subtitle:{
+        fontSize:16,
+    }
+})
